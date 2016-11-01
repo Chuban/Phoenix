6f52e5f10e8264c3931afaa93acb87ca7bdd2813
Adam Cahil <adam.cahill.ctr@afit.edu>
2016-11-01 10:11:34 -0400

    Beginning a new application

--- src/base/PhoenixApp.C
+++ src/base/PhoenixApp.C
@@ -1,11 +1,11 @@
-#include "StorkApp.h"
+#include "PhoenixApp.h"
 #include "Moose.h"
 #include "AppFactory.h"
 #include "ModulesApp.h"
 #include "MooseSyntax.h"
 
 template<>
-InputParameters validParams<StorkApp>()
+InputParameters validParams<PhoenixApp>()
 {
   InputParameters params = validParams<MooseApp>();
 
@@ -16,40 +16,40 @@ InputParameters validParams<StorkApp>()
   return params;
 }
 
-StorkApp::StorkApp(InputParameters parameters) :
+PhoenixApp::PhoenixApp(InputParameters parameters) :
     MooseApp(parameters)
 {
   Moose::registerObjects(_factory);
   ModulesApp::registerObjects(_factory);
-  StorkApp::registerObjects(_factory);
+  PhoenixApp::registerObjects(_factory);
 
   Moose::associateSyntax(_syntax, _action_factory);
   ModulesApp::associateSyntax(_syntax, _action_factory);
-  StorkApp::associateSyntax(_syntax, _action_factory);
+  PhoenixApp::associateSyntax(_syntax, _action_factory);
 }
 
-StorkApp::~StorkApp()
+PhoenixApp::~PhoenixApp()
 {
 }
 
 // External entry point for dynamic application loading
-extern "C" void StorkApp__registerApps() { StorkApp::registerApps(); }
+extern "C" void PhoenixApp__registerApps() { PhoenixApp::registerApps(); }
 void
-StorkApp::registerApps()
+PhoenixApp::registerApps()
 {
-  registerApp(StorkApp);
+  registerApp(PhoenixApp);
 }
 
 // External entry point for dynamic object registration
-extern "C" void StorkApp__registerObjects(Factory & factory) { StorkApp::registerObjects(factory); }
+extern "C" void PhoenixApp__registerObjects(Factory & factory) { PhoenixApp::registerObjects(factory); }
 void
-StorkApp::registerObjects(Factory & factory)
+PhoenixApp::registerObjects(Factory & factory)
 {
 }
 
 // External entry point for dynamic syntax association
-extern "C" void StorkApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory) { StorkApp::associateSyntax(syntax, action_factory); }
+extern "C" void PhoenixApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory) { PhoenixApp::associateSyntax(syntax, action_factory); }
 void
-StorkApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
+PhoenixApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
 {
 }
